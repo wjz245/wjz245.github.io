906a544128e935780d092d94a76c7525371ed545
wjz245 <wjc18223151760@gmail.com>
2024-07-07 09:38:58 +0800

    Site updated: 2024-07-07 09:38:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/06/2号/","2024/07/03/这是一篇新文章/","2024/07/03/hello-world/"];function toRandomPost(){
+var posts=["wenzhang/2号/","wenzhang/这是一篇新文章/","wenzhang/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
