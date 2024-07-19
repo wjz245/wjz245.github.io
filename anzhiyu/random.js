de396c7d17794697838746e83e05a1954d0fd5b2
wjz245 <wjc18223151760@gmail.com>
2024-07-19 15:06:09 +0800

    Site updated: 2024-07-19 15:06:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["wenzhang/jiyu/","wenzhang/Markdown的基础语法 No.1/","wenzhang/网页游戏/"];function toRandomPost(){
+var posts=["wenzhang/jiyu/","wenzhang/基于Windows的安卓子系统/","wenzhang/Markdown的基础语法 No.1/","wenzhang/网页游戏/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
