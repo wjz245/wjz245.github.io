97c6ef25e308afa57451d24db7afdbf2e5be63c1
wjz245 <wjc18223151760@gmail.com>
2024-08-10 22:20:28 +0800

    Site updated: 2024-08-10 22:20:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["wenzhang/jiyu/","wenzhang/基于Windows的安卓子系统/","wenzhang/网页游戏/"];function toRandomPost(){
+var posts=["wenzhang/jiyu/","wenzhang/基于Windows的安卓子系统/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
